608c125ff98ee411f0e36bf537a82ae0a89853a5
Kyle Isom <coder@kyleisom.net>
2013-06-14 19:14:47 -0600

    Improve KEK tests; add benchmarks.
    * The KEK test now generates a pair of key lists, and ensures that both
      lists get the same key (simulating two communicating parties).

--- dhkam_test.go
+++ dhkam_test.go
@@ -2,14 +2,19 @@ package dhkam
 
 import "bytes"
 import "crypto/rand"
-import "crypto/sha256"
+import "crypto/sha512"
 import "fmt"
 import "testing"
 
 // Generating a shared key for AES256 with an HMAC-SHA512 requires 96
-// bytes of keying material.
+// bytes of keying material. This is the largest key likely to be in use,
+// so we make sure we can at least generate this much key material.
 const SharedKeySize = 96
+const numTestCEK = 5 // number of test CEKs to generate
 
+// TestSharedKey validates the computation of a shared key between two
+// private keys. It generates two keys, computes the shared key between
+// both pairs, and ensures that those two shared keys are the same.
 func TestSharedKey(t *testing.T) {
 	prv1, err := GenerateKey(rand.Reader)
 	if err != nil {
@@ -39,6 +44,8 @@ func TestSharedKey(t *testing.T) {
 	}
 }
 
+// TestImportPrivate generates a key, exports it, and reimports the key,
+// verifying that it returns the same information.
 func TestImportPrivate(t *testing.T) {
 	prv, err := GenerateKey(rand.Reader)
 	if err != nil {
@@ -54,9 +61,14 @@ func TestImportPrivate(t *testing.T) {
 	} else if prv.X.Cmp(prv1.X) != 0 {
 		fmt.Println(ErrInvalidPrivateKey.Error())
 		t.Fail()
+	} else if prv.PublicKey.A.Cmp(prv1.PublicKey.A) != 0 {
+		fmt.Println("dhkam: private key import failed.")
+		t.Fail()
 	}
 }
 
+// TestImportPublic generates a key, exports the public key, and reimports
+// it, ensuring the two public keys match.
 func TestImportPublic(t *testing.T) {
 	prv, err := GenerateKey(rand.Reader)
 	if err != nil {
@@ -75,6 +87,10 @@ func TestImportPublic(t *testing.T) {
 	}
 }
 
+// TestKEK generates two private keys, computes a KEK from each, and
+// generates a pair of lists of CEKs between each pair of private keys. It
+// validates the uniqueness of those keys (i.e., the CEKs in a single
+// list should be unique) and that the two lists are identical.
 func TestKEK(t *testing.T) {
 	prv1, err := GenerateKey(rand.Reader)
 	if err != nil {
@@ -88,33 +104,118 @@ func TestKEK(t *testing.T) {
 		t.FailNow()
 	}
 
-	kek := InitialiseKEK(AES256CBC, SharedKeySize, nil)
-	if kek == nil {
+	kek1 := InitializeKEK(AES256CBC, SharedKeySize, nil)
+	if kek1 == nil {
 		fmt.Println("dhkam: failed to initialise KEK")
 		t.FailNow()
 	}
+	kek2 := InitializeKEK(AES256CBC, SharedKeySize, nil)
+	if kek2 == nil {
+		fmt.Println("dhkam: failed to initialise KEK")
+		t.FailNow()
+	}
+	pub1 := &prv1.PublicKey
+	pub2 := &prv2.PublicKey
 
-	pub := &prv2.PublicKey
-
-	keyList := make([][]byte, 5)
-	for i := 0; i < 5; i++ {
-		h := sha256.New()
-		keyList[i], err = prv1.CEK(rand.Reader, pub, kek, h)
+	keyList1 := make([][]byte, numTestCEK)
+	keyList2 := make([][]byte, numTestCEK)
+	for i := 0; i < numTestCEK; i++ {
+		h := sha512.New()
+		keyList1[i], err = prv1.CEK(rand.Reader, pub2, kek1, h)
+		if err != nil {
+			fmt.Println(err.Error())
+			t.FailNow()
+		}
+		keyList2[i], err = prv2.CEK(rand.Reader, pub1, kek2, h)
 		if err != nil {
 			fmt.Println(err.Error())
 			t.FailNow()
 		}
 	}
 
-	for i := 0; i < 5; i++ {
-		for j := 0; j < 5; j++ {
+	for i := 0; i < numTestCEK; i++ {
+		for j := 0; j < numTestCEK; j++ {
 			if i == j {
 				continue
 			}
-			if bytes.Equal(keyList[i], keyList[j]) {
+			if bytes.Equal(keyList1[i], keyList1[j]) {
+				fmt.Println("dhkam: CEK isn't unique")
+				t.FailNow()
+			}
+			if bytes.Equal(keyList2[i], keyList2[j]) {
 				fmt.Println("dhkam: CEK isn't unique")
 				t.FailNow()
 			}
 		}
 	}
+
+	for i := 0; i < numTestCEK; i++ {
+		if !bytes.Equal(keyList1[i], keyList2[i]) {
+			fmt.Println("dhkam: CEK's don't match")
+			t.FailNow()
+		}
+	}
+}
+
+// Benchmark the generate of private keys.
+func BenchmarkGenerateKey(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := GenerateKey(rand.Reader)
+		if err != nil {
+			fmt.Println(err.Error())
+			b.FailNow()
+		}
+	}
+}
+
+// Benchmark the computation of shared keys.
+func BenchmarkSharedKey(b *testing.B) {
+	prv1, err := GenerateKey(rand.Reader)
+	if err != nil {
+		fmt.Println(err.Error())
+		b.FailNow()
+	}
+	prv2, err := GenerateKey(rand.Reader)
+	if err != nil {
+		fmt.Println(err.Error())
+		b.FailNow()
+	}
+	for i := 0; i < b.N; i++ {
+		_, err := prv1.SharedKey(rand.Reader, &prv2.PublicKey, SharedKeySize)
+		if err != nil {
+			fmt.Println(err.Error())
+			b.FailNow()
+		}
+	}
+
+}
+
+// Benchmark the generation of CEKs.
+func BenchmarkCEKGeneration(b *testing.B) {
+	prv1, err := GenerateKey(rand.Reader)
+	if err != nil {
+		fmt.Println(err.Error())
+		b.FailNow()
+	}
+	prv2, err := GenerateKey(rand.Reader)
+	if err != nil {
+		fmt.Println(err.Error())
+		b.FailNow()
+	}
+	kek := InitializeKEK(AES256CBC, SharedKeySize, nil)
+	if kek == nil {
+		fmt.Println("dhkam: failed to generate KEK")
+		b.FailNow()
+	}
+
+	h := sha512.New()
+
+	for i := 0; i < b.N; i++ {
+		_, err := prv1.CEK(rand.Reader, &prv2.PublicKey, kek, h)
+		if err != nil {
+			fmt.Println(err.Error())
+			b.FailNow()
+		}
+	}
+
 }
